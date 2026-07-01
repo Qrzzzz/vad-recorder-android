71712a389b83db4178dc5db1dece918db0410d70
Qrzzzz <qrzzzz1412@gmail.com>
2026-07-02 00:51:30 +0800

    chore: name generated APK by version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,6 +26,14 @@ android {
         }
     }
 
+    applicationVariants.all {
+        val variant = this
+        outputs.all {
+            val output = this as com.android.build.gradle.internal.api.BaseVariantOutputImpl
+            output.outputFileName = "VADRecorder-v${variant.versionName}.apk"
+        }
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
